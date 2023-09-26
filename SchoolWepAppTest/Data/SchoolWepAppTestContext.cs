137dff40c025a34f09f63b144f629fd8c4dda870
Abdalmunam Abdalla <abdalmenam1980@gmail.com>
2023-09-26 13:36:25 +0200

    Add project files.

---
+++ SchoolWepAppTest/Data/SchoolWepAppTestContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SchoolWepAppTest.Models;
+
+namespace SchoolWepAppTest.Data
+{
+    public class SchoolWepAppTestContext : DbContext
+    {
+        public SchoolWepAppTestContext (DbContextOptions<SchoolWepAppTestContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SchoolWepAppTest.Models.Student> Student { get; set; } = default!;
+    }
+}
---
+++ SchoolWepAppTest/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolWepAppTest.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name="Student Name")]
+        public string Name { get; set; } = string.Empty;
+        [DataType(DataType.Date)]
+        [Display(Name="Date of Birth")]
+        public DateTime DoB { get; set; }
+    }
+}
